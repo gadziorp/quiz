4e544765f817baeaf6073a81173c0218356e9b32
Piotr <gadziorp@gmail.com>
2018-03-22 20:00:29 +0100

    Change to public variables not methods.

--- app/src/main/java/com/example/android/quiz/MainActivity.java
+++ app/src/main/java/com/example/android/quiz/MainActivity.java
@@ -12,31 +12,42 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    int score4 = 0;
+    int score3 = 0;
+    int score2 = 0;
+    int score1 = 0;
+    int score5 = 0;
+    int score6 = 0;
+    int result = 0;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    // method to display result
-    public void displayScore() {
-        int score = calculateResult(question4score(), question3score(), question2score(), question1score(), question5score(), question6score());
-        TextView viewScore = findViewById(R.id.result);
-        viewScore.setText(score);
     }
 
     // method to calculate result
-    private int calculateResult(int question4score, int question3score, int question2score, int question1score, int question5score, int question6score) {
-        int scoreCalc = +question4score + question3score + question2score + question1score + question5score + question6score;
-        return scoreCalc;
-    }
-
-    public int question4score() {
-        int score4 = 0;
-        // Is the button now checked?
+    public void checkResult(View view) {
         RadioGroup Question4 = findViewById(R.id.fourthQuestionRadioGroup);
         RadioButton CorrectAnswer4 = findViewById(R.id.radioButton2Question4);
-        // checking answer
+        RadioGroup Question3 = findViewById(R.id.thirdQuestionRadioGroup);
+        RadioButton CorrectAnswer3 = findViewById(R.id.radioButton2Question3);
+        RadioGroup Question2 = findViewById(R.id.secondQuestionRadioGroup);
+        RadioButton CorrectAnswer2 = findViewById(R.id.radioButton2Question2);
+        RadioGroup Question1 = findViewById(R.id.firstQuestionRadioGroup);
+        RadioButton CorrectAnswer1 = findViewById(R.id.radioButton3);
+        //        check the checkboxes
+        boolean brown = ((CheckBox) findViewById(R.id.checkBox1Question5)).isChecked();
+        boolean black = ((CheckBox) findViewById(R.id.checkBox2Question5)).isChecked();
+        boolean grey = ((CheckBox) findViewById(R.id.checkBox3Question5)).isChecked();
+        //convert answer so string
+        EditText edt = findViewById(R.id.editText);
+        String answer = edt.getText().toString();
+        // Is the button now checked?
+
         if (Question4.getCheckedRadioButtonId() == -1) {
 
         } else {
@@ -48,14 +59,9 @@ public class MainActivity extends AppCompatActivity {
             if (selectedRadioButton == CorrectAnswer4)
                 score4 = 1;
         }
-        return score4;
-    }
 
-    public int question3score() {
-        int score3 = 0;
         // Is the button now checked?
-        RadioGroup Question3 = findViewById(R.id.thirdQuestionRadioGroup);
-        RadioButton CorrectAnswer3 = findViewById(R.id.radioButton2Question3);
+
         if (Question3.getCheckedRadioButtonId() == -1) {
 
         } else {
@@ -67,14 +73,7 @@ public class MainActivity extends AppCompatActivity {
             if (selectedRadioButton == CorrectAnswer3)
                 score3 = 1;
         }
-        return score3;
-    }
-
-    public int question2score() {
-        int score2 = 0;
         // Is the button now checked?
-        RadioGroup Question2 = findViewById(R.id.secondQuestionRadioGroup);
-        RadioButton CorrectAnswer2 = findViewById(R.id.radioButton2Question2);
         if (Question2.getCheckedRadioButtonId() == -1) {
 
         } else {
@@ -86,14 +85,7 @@ public class MainActivity extends AppCompatActivity {
             if (selectedRadioButton == CorrectAnswer2)
                 score2 = 1;
         }
-        return score2;
-    }
 
-    public int question1score() {
-        int score1 = 0;
-
-        RadioGroup Question1 = findViewById(R.id.firstQuestionRadioGroup);
-        RadioButton CorrectAnswer1 = findViewById(R.id.radioButton3);
         // Is the button now checked?
         if (Question1.getCheckedRadioButtonId() == -1) {
 
@@ -106,30 +98,23 @@ public class MainActivity extends AppCompatActivity {
             if (selectedRadioButton == CorrectAnswer1)
                 score1 = 1;
         }
-        return score1;
-    }
 
-    public int question5score() {
-        int score5 = 0;
-//        check the checkboxes
-        boolean brown = ((CheckBox) findViewById(R.id.checkBox1Question5)).isChecked();
-        boolean black = ((CheckBox) findViewById(R.id.checkBox2Question5)).isChecked();
-        boolean grey = ((CheckBox) findViewById(R.id.checkBox3Question5)).isChecked();
-// loop for checking right answer
+// loop for checking right answer with checkbox questions
         if (black && brown && grey)
             score5 = 1;
-        return score5;
-    }
 
-    public int question6score() {
-        int score6 = 0;
-        EditText edt = findViewById(R.id.editText);
-        String answer = edt.getText().toString();
-
+        // checking text in editText View with correct answer
         if (answer.equals("Peru")) {
             score6 = 1;
         }
-        return score6;
+        result = score1 + score2 + score3 + score4 + score5 + score6;
+        displayScore(result);
+    }
+
+    // method to display result
+    public void displayScore(int result) {
+        TextView viewScore = findViewById(R.id.result);
+        viewScore.setText(result);
     }
 }
 
